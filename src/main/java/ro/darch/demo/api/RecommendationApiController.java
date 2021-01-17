b66ef95931e7334a3ae6ba6f1a9e2cfff0e39a20
Grama Ioan-Emanuel <gramai.emanuel@gmail.com>
2021-01-17 21:39:04 +0200

    Allowed cross origin

--- src/main/java/ro/darch/demo/api/RecommendationApiController.java
+++ src/main/java/ro/darch/demo/api/RecommendationApiController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
 import ro.darch.demo.model.GetRecommendationResponseBody200;
 import ro.darch.demo.service.RecommendationService;
@@ -20,6 +21,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 @Slf4j
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCustomCodegen", date = "2021-01-17T19:13:55.210+02:00")
+@CrossOrigin
 public class RecommendationApiController implements RecommendationApi {
     private final ObjectMapper objectMapper;
 
@@ -44,7 +46,7 @@ public class RecommendationApiController implements RecommendationApi {
 
     @Override
     public ResponseEntity<List<GetRecommendationResponseBody200>> getRecommendation(@ApiParam(value = "Name of the match") @Valid @RequestParam(value = "name", required = false) String name, @ApiParam(value = "Area of the Match") @Valid @RequestParam(value = "area", required = false) String area, @ApiParam(value = "Programming Language of the Match") @Valid @RequestParam(value = "programmingLanguage", required = false) String programmingLanguage, @ApiParam(value = "Type of the Match") @Valid @RequestParam(value = "type", required = false) String type) {
-        return new ResponseEntity<>(recommendationService.getRecommendation(name,area, programmingLanguage, type), HttpStatus.OK);
+        return new ResponseEntity<>(recommendationService.getRecommendation(name, area, programmingLanguage, type), HttpStatus.OK);
     }
 
 }
